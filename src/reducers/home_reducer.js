e808e720ed5b2beb7f3b25386b747edd6b82c2f6
Ruth Newman <ruthnewman@gmail.com>
2020-10-09 00:41:56 +0100

    added default

--- src/reducers/home_reducer.js
+++ src/reducers/home_reducer.js
@@ -56,5 +56,7 @@ export default function(state = defaultState, action) {
                 ...state,
                 loading: true
             }
+        default:
+            return state;
     }
 }
