ee482966332d032e87fd45ea99d3a31b4e0386b2
Ruth Newman <ruthnewman@gmail.com>
2020-10-09 00:40:59 +0100

    added home reducer

--- src/reducers/home_reducer.js
+++ src/reducers/home_reducer.js
@@ -1,3 +1,13 @@
+const { SHOW_LOADING_SPINNER, CLEAR_MOVIES, SEARCH_FOR_MOVIES, GET_POPULAR_MOVIES, RENDER_MORE_MOVIES } = require("../actions");
+
+import {
+    SHOW_LOADING_SPINNER,
+    CLEAR_MOVIES,
+    SEARCH_FOR_MOVIES,
+    GET_POPULAR_MOVIES,
+    RENDER_MORE_MOVIES
+} from '../actions';
+
 const defaultState = {
     movies: [],
     heroImage: null,
@@ -6,3 +16,45 @@ const defaultState = {
     totalPages: 0,
     searchTerm: ''
 };
+
+export default function(state = defaultState, action) {
+    switch(action.type) {
+        case GET_POPULAR_MOVIES:
+            return {
+                ...state,
+                movies: action.payload.results,
+                heroImage: state.heroImage || action.payload.results[0],
+                loading: false,
+                currentPage: action.payload.page,
+                totalPages: action.payload.total_pages,
+                searchTerm: ''
+            }
+        case RENDER_MORE_MOVIES:
+            return {
+                ...state,
+                movies: [...state.movies, ...action.payload.results],
+                loading: false,
+                currentPage: action.payload.page,
+                totalPages: action.payload.total_pages
+            }
+        case SEARCH_FOR_MOVIES:
+            return {
+                ...state,
+                movies: action.payload.results,
+                loading: false,
+                currentPage: action.payload.page,
+                totalPages: action.payload.total_pages,
+                searchTerm: action.payload.searchTerm
+            }
+        case CLEAR_MOVIES:
+            return {
+                ...state,
+                moves: []
+            }
+        case SHOW_LOADING_SPINNER:
+            return {
+                ...state,
+                loading: true
+            }
+    }
+}
